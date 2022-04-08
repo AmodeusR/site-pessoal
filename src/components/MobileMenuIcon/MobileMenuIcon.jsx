d856c2b165bc3a3950076e36430668d39b88ddf5
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-04-08 15:33:05 -0300

    finished Navigation

--- src/components/MobileMenuIcon/MobileMenuIcon.jsx
+++ src/components/MobileMenuIcon/MobileMenuIcon.jsx
@@ -3,7 +3,7 @@ import "./mobile-menu.scss";
 const MobileMenuIcon = () => {
   return (
     <>
-      <div className="mobile-menu">
+      <div className="mobile-menu" data-type="mobile-menu">
         <div className="mobile-menu__top-line" data-type="mobile-menu" />
         <div className="mobile-menu__middle-line" data-type="mobile-menu" />
         <div className="mobile-menu__bottom-line" data-type="mobile-menu" />
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,18 +1,40 @@
-// import React from "react";
-// import { assets } from "../../constants";
-import {ReactComponent as Logo} from "../../assets/icons/logo.svg";
-
-
+import { useState } from "react";
+import { assets } from "../../constants";
+import { MobileMenuIcon, MobileCloseMenuIcon } from "../../components";
 
 import "./navigation.scss";
+const { Logo } = assets;
 
 const Navigation = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const handleMenuToggle = ({ target }) => {
+    const clickType = target.dataset.type;
+
+    switch(clickType) {
+    case "mobile-menu":
+      setIsMobileMenuOpen(true);
+      break;
+
+    case "overlay":
+    case "close":
+      setIsMobileMenuOpen(false);
+      break;
+    }
+  };
+
   return (
-    <header className="header">
+    <header className={`header ${isMobileMenuOpen ? "header--open" : ""}`} onClick={handleMenuToggle}>
+      <div className="overlay" data-type="overlay" />
       <div className="inner-container header__flex">
         <Logo className="header__logo" />
-        <nav className="header__nav">
+        <nav className="header__nav" data-type="mobile-menu">
+          <MobileMenuIcon />
           <ul className="header__list">
+            <div className="menu-header">
+              <span className="list-title menu-header__title">Menu</span>
+              <MobileCloseMenuIcon />
+            </div>
             <li className="header__list-items"><a href="#" className="header__link">Início</a></li>
             <li className="header__list-items"><a href="#" className="header__link">Sobre Mim</a></li>
             <li className="header__list-items"><a href="#" className="header__link">Projetos</a></li>
@@ -20,6 +42,7 @@ const Navigation = () => {
           </ul>
         </nav>
       </div>
+
     </header>
   );
 };
