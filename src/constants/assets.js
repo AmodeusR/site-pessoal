8d6eeb4dc66a7b97805ba581fe7a409cd1a88c20
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-04-08 14:30:37 -0300

    renamed icon to download

--- src/constants/assets.js
+++ src/constants/assets.js
@@ -1,5 +1,29 @@
-import logo from "../assets/icons/logo.svg";
+import { ReactComponent as Logo} from "../assets/icons/logo.svg";
+import { ReactComponent as DownloadIcon} from "../assets/icons/download.svg";
+
+import { ReactComponent as FigmaIcon} from "../assets/icons/apps/figma.svg";
+import { ReactComponent as IllustratorIcon} from "../assets/icons/apps/illustrator.svg";
+import { ReactComponent as PhotoshopIcon} from "../assets/icons/apps/photoshop.svg";
+import { ReactComponent as PremiereProIcon} from "../assets/icons/apps/premiere-pro.svg";
+import { ReactComponent as VSCodeIcon} from "../assets/icons/apps/vs-code.svg";
+
+import { ReactComponent as HTMLIcon} from "../assets/icons/techs/html.svg";
+import { ReactComponent as CSSIcon} from "../assets/icons/techs/css.svg";
+import { ReactComponent as JSIcon} from "../assets/icons/techs/javascript.svg";
+import { ReactComponent as ReactIcon} from "../assets/icons/techs/react.svg";
+import { ReactComponent as SassIcon} from "../assets/icons/techs/sass.svg";
 
 export default {
-  logo
+  Logo,
+  DownloadIcon,
+  FigmaIcon,
+  IllustratorIcon,
+  PhotoshopIcon,
+  PremiereProIcon,
+  VSCodeIcon,
+  HTMLIcon,
+  CSSIcon,
+  JSIcon,
+  ReactIcon,
+  SassIcon
 };
