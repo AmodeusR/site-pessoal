13b64c10eec19f2e64fc5b1aff5e5a927aa658a8
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-04-08 14:31:32 -0300

    Created MobileMenuIcon component

---
+++ src/components/MobileMenuIcon/MobileMenuIcon.jsx
@@ -0,0 +1,15 @@
+import "./mobile-menu.scss";
+
+const MobileMenuIcon = () => {
+  return (
+    <>
+      <div className="mobile-menu">
+        <div className="mobile-menu__top-line" data-type="mobile-menu" />
+        <div className="mobile-menu__middle-line" data-type="mobile-menu" />
+        <div className="mobile-menu__bottom-line" data-type="mobile-menu" />
+      </div>
+    </>
+  );
+};
+
+export default MobileMenuIcon;
