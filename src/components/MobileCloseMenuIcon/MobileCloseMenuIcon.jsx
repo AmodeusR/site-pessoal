e501297c6e3916f8a849933cc39effb550c69ec5
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-04-08 14:31:08 -0300

    created MobileCloseMenuIcon component

---
+++ src/components/MobileCloseMenuIcon/MobileCloseMenuIcon.jsx
@@ -0,0 +1,12 @@
+import "./close-menu.scss";
+
+const MobileCloseMenuIcon = () => {
+  return (
+    <div className="close-menu" data-type="close">
+      <div className="line line-1" data-type="close" />
+      <div className="line line-2" data-type="close" />
+    </div>
+  );
+};
+
+export default MobileCloseMenuIcon;
