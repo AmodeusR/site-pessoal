f9dbec2c465cf099c66c192d740c97396abd0d60
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-04-09 23:14:54 -0300

    Finished Hero Section

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -6,10 +6,12 @@ const HeroSection = () => {
     <div className="outer-container hero">
       <h1 className="hero__title"><span className="theme-color">Front-end</span> Dev & <span className="theme-color">UI/UX</span> Designer</h1>
       <span className="hero__description">
-        Moldando o mundo através da <span className="theme-color" style={{fontWeight: 700}}>programação</span>
+        Moldando o mundo através da <span className="theme-color" style={{fontWeight: 700, color: "var(--gradiente-origem)"}}>programação</span>
       </span>
       <div className="hero__cta">
-        <Button title="Projetos" />
+        <a href="#projetos" id="link-project">
+          <Button title="Projetos" />
+        </a>
         <Button title="Baixar CV" className="button--hollow" type="download" />
       </div>
     </div>
