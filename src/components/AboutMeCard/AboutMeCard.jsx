2eb7d30525ab491358ba39f1312a8b508f94a383
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-04-12 18:05:03 -0300

    Created AboutMeCard component

---
+++ src/components/AboutMeCard/AboutMeCard.jsx
@@ -0,0 +1,28 @@
+import { Button } from "../../components";
+import "./about-card.scss";
+
+const AboutMeCard = () => {
+  return (
+    <div className="about-card">
+      <img
+        className="about-card__img"
+        src="https://source.unsplash.com/random"
+        alt="picture of me"
+      />
+      <h3 className="about-card__title">Ricardo Magalhães (A.R)</h3>
+      <p className="about-card__description">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere
+        accusamus sit quas nostrum! Laudantium quo saepe earum corrupti sequi
+        vel odio perspiciatis, iste vitae ut itaque reprehenderit quaerat nemo
+        minus.
+      </p>
+      <Button
+        className="button--hollow button--disabled about-card__button"
+        title="Mais sobre mim"
+        type="arrow"
+      />
+    </div>
+  );
+};
+
+export default AboutMeCard;
