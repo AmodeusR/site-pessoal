96e711eaa68cd638ebc7be7e7386c7a797300269
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-04-12 18:04:39 -0300

    moved HeroSection from Components to Sections folder

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { default as Navigation } from "./Navigation/Navigation";
 export { default as MobileMenuIcon } from "./MobileMenuIcon/MobileMenuIcon";
 export { default as MobileCloseMenuIcon } from "./MobileCloseMenuIcon/MobileCloseMenuIcon";
-export { default as HeroSection } from "./HeroSection/HeroSection";
 export { default as Button } from "./Button/Button";
+export { default as AboutMeCard } from "./AboutMeCard/AboutMeCard";
---
+++ src/sections/Hero/Hero.jsx
@@ -0,0 +1,25 @@
+import "./hero-section.scss";
+import { Button } from "../../components";
+
+const Hero = () => {
+  return (
+    <div className="outer-container hero" id="home">
+      <h1 className="hero__title theme-container">
+        Front-end <span className="theme-neutralize">Dev &</span><br /> UI/UX{" "}
+        <span className="theme-neutralize">Designer</span>
+      </h1>
+      <span className="hero__description">
+        Moldando o mundo através da{" "}<br />
+        <span className="modular-text">programação.</span>
+      </span>
+      <div className="hero__cta">
+        <a href="#projetos" id="link-project">
+          <Button title="Projetos" />
+        </a>
+        <Button title="Baixar CV" className="button--hollow" type="download" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
