be3a250de7e94c7d82244c635ebb867169667db5
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-04-14 19:14:29 -0300

    no focus on disabled Button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -4,37 +4,44 @@ import { assets } from "../../constants";
 
 const { DownloadIcon, ArrowIcon } = assets;
 
-const Button = ({ title, className = "", type, ...options }) => {
+const Button = ({
+  title,
+  className = "",
+  type,
+  disabled = false,
+  ...options
+}) => {
   const iconToRender = getIconToRender(type);
 
-
-  console.log(!!iconToRender);
-  
   return (
-    <button className={`button ${className}`} type="button" {...options}>
+    <button
+      className={`button ${className} ${disabled ? "button--disabled" : ""}`}
+      tabIndex={disabled ? -1 : 0}
+      type="button"
+      {...options}
+    >
       {type ? iconToRender : ""}
       {title}
     </button>
   );
 };
 
-
 export default Button;
 
 function getIconToRender(type) {
   let iconToRender;
 
-  switch(type) {
-  case "download":
-    iconToRender = <DownloadIcon />;
-    break;
-  
-  case "arrow":
-    iconToRender = <ArrowIcon />;
-    break;
-  
-  default:
-    iconToRender = null;
+  switch (type) {
+    case "download":
+      iconToRender = <DownloadIcon />;
+      break;
+
+    case "arrow":
+      iconToRender = <ArrowIcon />;
+      break;
+
+    default:
+      iconToRender = null;
   }
 
   return iconToRender;
