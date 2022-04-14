1de7041c8fe793ba1421368ee5cabda8736c6abe
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-04-14 19:15:13 -0300

    started styling About Section

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -1,4 +1,5 @@
 import { AboutMeCard } from "../../components";
+import Knowledge from "./Knowledge/Knowledge";
 
 import "./about-section.scss";
 
@@ -7,7 +8,8 @@ const About = () => {
     <section className="about outer-container">
       <h2 className="section-title">Sobre Mim<span className="theme-container">_</span></h2>
       <div className="about-grid">
-        <AboutMeCard />
+        <AboutMeCard className="about__card" />
+        <Knowledge />
       </div>
     </section>
   );
---
+++ src/sections/About/Knowledge/Knowledge.jsx
@@ -0,0 +1,9 @@
+const Knowledge = () => {
+  return (
+    <section className="knowledge inner-container">
+      <h3 className="section-subtitle-l1 knowledge__title">Conhecimentos</h3>
+    </section>
+  );
+};
+
+export default Knowledge;
