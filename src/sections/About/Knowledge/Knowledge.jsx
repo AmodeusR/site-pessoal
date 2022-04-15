670237b23516c32a53c8563435a9404dac76ea5c
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-04-14 21:17:58 -0300

    base fetch from Sanity

--- src/sections/About/Knowledge/Knowledge.jsx
+++ src/sections/About/Knowledge/Knowledge.jsx
@@ -1,4 +1,24 @@
+import { useState, useEffect } from "react";
+import { urlFor, client } from "../../../utils/sanityClient";
+
 const Knowledge = () => {
+  const [techs, setTechs] = useState([]);
+  useEffect(() => {
+    const query = "*[_type == 'techs']";
+    const fetchData = async () => {
+      try {
+        const data = await client.fetch(query);
+        console.log(data);
+        setTechs(data);
+        
+      } catch (error) {
+        console.warn(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <section className="knowledge inner-container">
       <h3 className="section-subtitle-l1 knowledge__title">Conhecimentos</h3>
