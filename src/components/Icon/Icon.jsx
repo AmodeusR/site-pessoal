1fce62bad4a02331795d9605f55c7f9ce5edf6d8
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-04-20 16:39:49 -0300

    Update Icon.jsx

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,6 +1,6 @@
 import { urlFor } from "../../utils/sanityClient";
 
-const Icon = ({ src, alt, className = "" }) => {
+const Icon = ({ src, alt, className = null }) => {
   return <img src={urlFor(src)} alt={alt} className={className} />;
 };
 
