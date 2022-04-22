742158c4a25d6553ff0bb701935bf6a2de1a24e3
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-04-21 21:42:18 -0300

    added link reference to techs & apps

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,7 +1,11 @@
 import { urlFor } from "../../utils/sanityClient";
 
-const Icon = ({ src, alt, className = null }) => {
-  return <img src={urlFor(src)} alt={alt} className={className} />;
+const Icon = ({ src, alt, className = null, linkRef = "#/" }) => {
+  return (
+    <a href={linkRef}>
+      <img src={urlFor(src)} alt={alt} className={className} />
+    </a>
+  );
 };
 
 export default Icon;
