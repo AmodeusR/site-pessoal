6e8c8882a405963adf8636a02e6d4fe663c93358
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-04-21 21:38:35 -0300

    finished & improved Footer styles

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -9,24 +9,27 @@ const Footer = () => {
       <LogoGrayscale />
       <div className="footer__section">
         <h2 className="footer__title">Início</h2>
-        <a href="#about-me" className="footer__link">
-          Sobre Mim
-        </a>
-        <a href="#projects" className="footer__link">
-          Projetos
-        </a>
+        <div className="footer__links">
+          <a href="#about-me" className="footer__link">
+            Sobre Mim
+          </a>
+          <a href="#projects" className="footer__link">
+            Projetos
+          </a>
+        </div>
       </div>
       <div className="footer__section social">
         <h2 className="footer__title">Social</h2>
         <div className="footer__icons">
-          <a href="https://github.com/amodeusr">
+          <a href="https://github.com/amodeusr" target="_blank" rel="noreferrer">
             <GithubIcon />
           </a>
-          <a href="https://linkedin.com/in/amodeusr">
+          <a href="https://linkedin.com/in/amodeusr" target="_blank" rel="noreferrer">
             <LinkedinIcon />
           </a>
         </div>
       </div>
+      <p className="footer__section footer__description">Este site foi desenvolvido por Amodeus R.</p>
     </footer>
   );
 };
