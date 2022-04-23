9b18701ea2abd83b5f31dc9d29a906cb77b5ab7d
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-04-22 21:55:43 -0300

    Finished Contact Form config

--- src/sections/Contact/ContactCard/ContactCard.jsx
+++ src/sections/Contact/ContactCard/ContactCard.jsx
@@ -1,22 +1,57 @@
-import { Input, Button } from "../../../components";
+import { useState, useEffect } from "react";
+import { useForm } from "@formspree/react";
+
+import { Input, Button, LoadingAnimation } from "../../../components";
 import "./contact-card.scss";
 
 const ContactCard = () => {
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-  };
+
+  const [state, handleFormSubmit] = useForm("xyyobgqr");
+  const [buttonContent, setButtonContent] = useState("Enviar mensagem");
+  const { submitting, succeeded, errors } = state;
+
+  useEffect(() => {
+    if (submitting) {
+      setButtonContent(<LoadingAnimation />);
+    } else if (errors.length) {
+      setButtonContent("Algo deu errado :(");
+      errors.forEach(error => {
+        console.warn("Formspree Error:");
+        console.warn(error);
+      });
+    } else if (succeeded) {
+      setButtonContent("Enviado!");
+    }
+
+  }, [state]);
 
   return (
     <div className="contact-card">
-      <h2 className="section-subtitle-l2 contact-card__title">Quer que eu trabalhe pra você? Vamos conversar então :D</h2>
-      <form className="contact-card__form" action="post" onSubmit={handleFormSubmit}>
-        <Input title="Seu nome" type="text" name="nome" />
-        <Input title="Seu e-mail" type="email" name="email" />
+      <h2 className="section-subtitle-l2 contact-card__title">
+        Quer que eu trabalhe pra você? Vamos conversar então :D
+      </h2>
+      <form
+        className="contact-card__form"
+        method="POST"
+        onSubmit={handleFormSubmit}
+      >
+        <Input title="Seu nome" type="text" name="nome"  required />
+        <Input title="Seu e-mail" type="email" name="email" required />
         <div className="inputlabel">
           <label htmlFor="mensagem">Sua mensagem</label>
-          <textarea className="contact-card__textfield" name="mensagem" id="mensagem"></textarea>
+          <textarea
+            
+            className="contact-card__textfield"
+            name="mensagem"
+            id="mensagem"
+            required
+          ></textarea>
         </div>
-        <Button title="Enviar mensagem" type="submit" className="contact-card__button" />
+        <Button
+          title={buttonContent}
+          type="submit"
+          className="contact-card__button"
+        />
       </form>
     </div>
   );
