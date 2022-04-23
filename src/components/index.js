0411f36602b8d064d78edcb8c843751de046d3b7
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-04-22 21:54:30 -0300

    Created LoadingAnimation
    Loading animation to Form Button

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,4 @@ export { default as Button } from "./Button/Button";
 export { default as Icon } from "./Icon/Icon";
 export { default as Project } from "./Project/Project";
 export { default as Input } from "./Input/Input";
+export { default as LoadingAnimation } from "./LoadingAnimation/LoadingAnimation";
