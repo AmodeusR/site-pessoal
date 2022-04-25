4b6363e843ad5766890577767b16ca3d29930770
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-04-24 23:45:30 -0300

    Updated & finished Projects component

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,17 +1,36 @@
+import { useState, useEffect } from "react";
 import { Project, Button } from "../../components";
+
+import { sanityFetchProjects } from "../../utils/sanityClient";
 
 import "./projects.scss";
 
 const Projects = () => {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const fetchedProjects = await sanityFetchProjects();
+
+      setProjects(fetchedProjects);
+    })();
+  }, []);
+
   return (
     <section className="projects outer-container" id="projects">
       <h2 className="section-title">Projetos</h2>
       <div className="projects__list">
-        <Project />
-        <Project />
-        <Project />
-        <Project />
-        <Project />
+        {projects?.map(({ _id, imageSrc, title, description, usedTechs, repoLink, demoLink }) => (
+          <Project
+            backgroundImage={imageSrc}
+            title={title}
+            description={description[0]}
+            usedTechs={usedTechs}
+            key={_id}
+            repoLink={repoLink}
+            demoLink={demoLink}
+          />
+        ))}
       </div>
       <Button title="Mais projetos" className="projects__button" />
     </section>
