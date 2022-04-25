eb610a781da8da113f3767ffcaea69b8bea1151d
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-04-24 23:45:00 -0300

    added target blank to Icon link

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -2,7 +2,7 @@ import { urlFor } from "../../utils/sanityClient";
 
 const Icon = ({ src, alt, className = null, linkRef = "#/" }) => {
   return (
-    <a href={linkRef}>
+    <a href={linkRef} target="_blank" rel="noreferrer">
       <img src={urlFor(src)} alt={alt} className={className} />
     </a>
   );
